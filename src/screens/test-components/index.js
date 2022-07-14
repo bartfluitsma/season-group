69a2c4e1236eca3108f3e68df1e7f857f472f1f2
bart_fluitsma <bart.fluitsma@wecreate.com.hk>
2022-07-14 15:42:24 +0200

    button components and test page

---
+++ src/screens/test-components/index.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import Button from '../../components/button'
+import ButtonCircle from '../../components/button-circle'
+import { withTrans } from '../../i18n/withTrans'
+import './test-components.scss'
+
+const TestComponents = ({ t }) => (
+  <>
+    <div className='testContainer'>
+      <h1>Test components</h1>
+      <Button text="Work with us" link="/" />
+      <Button text="Work with us" light={true} />
+      <br />
+      <ButtonCircle link='/'/>
+      <ButtonCircle link='/' leftArrow={true}/>
+      <ButtonCircle leftArrow={true} light={true} darkArrow={true} />
+      <ButtonCircle leftArrow={true} light={true} />
+      <ButtonCircle transparent={true} />
+      <ButtonCircle white={true} />
+    </div>
+  </>
+)
+
+export default withTrans(TestComponents)
